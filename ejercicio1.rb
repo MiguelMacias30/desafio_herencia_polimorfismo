40c106f9d906823f6a511413434013d03b07c452
Miguel Macias <miguelfernandomacias@gmail.com>
2020-09-13 20:52:19 -0400

    class teacher

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -26,4 +26,19 @@ class Student < Person
     def introduce
         puts "hola profesor mi nombre es: #{@first_name} #{@last_name}"
     end
+end
+
+class Teacher < Person
+
+    def initialize(first, last, age, type)
+        super(first, last, age, type)
+    end
+
+    def talk
+        puts 'Bienvenidos a la clase de programacion con ruby'
+    end
+
+    def introduce
+        puts "hola alumnos. mi nombre es #{@first_name} #{@last_name}"
+    end
 end 
